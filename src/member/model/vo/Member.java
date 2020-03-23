b4aeca7f536bd39d29962a646acf2bc4d8509bc5
hailey <hailey@192.168.130.41>
2020-03-23 21:42:33 +0900

    member
    member

--- src/member/model/vo/Member.java
+++ src/member/model/vo/Member.java
@@ -1,5 +1,67 @@
 package member.model.vo;
 
 public class Member {
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private int age;
+	private char gender;
+	private String email;
+	
+	public Member() {}
+	
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	public String getUserId() {
+		return userId;
+	}
+	
+	public String getUserPwd() {
+		return userPwd;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public char getGender() {
+		return gender;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	
+	
+	
+	
 
 }
