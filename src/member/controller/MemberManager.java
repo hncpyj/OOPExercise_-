2a401d0377d0e8987c384ac505c28854646a0274
hailey <hailey@192.168.130.41>
2020-03-23 23:15:54 +0900

    MemberMenu, Main 수정
    MemberMenu와 Menu 확인을 위한 Main 작성 코드 커

--- src/member/controller/MemberManager.java
+++ src/member/controller/MemberManager.java
@@ -1,5 +1,60 @@
 package member.controller;
 
-public class MemberManager {
+import java.util.Scanner;
 
+import member.model.vo.Member;
+
+public class MemberManager {
+	private Member[] m = new Member[10];
+	private int ctn = 0;
+	private Scanner sc = new Scanner(System.in);
+	
+	public MemberManager() {}
+	
+	public void insertMember() {
+		
+	}
+	
+	public void searchId() {
+		
+	}
+	
+	public void searchName() {
+		
+	}
+	
+	public void searchEmail() {
+		
+	}
+	
+	public void updatePwd() {
+		
+	}
+	
+	public void updateName() {
+		
+	}
+	
+	public void updateEmail() {
+		
+	}
+	
+	public void deleteOne() {
+		
+	}
+	
+	public void deleteAll() {
+		
+	}
+	
+	public void printAllMember() {
+		
+	}
+	
+	public void printOne() {
+		
+	}
+	
+	
+	
 }
